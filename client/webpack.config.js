2c6646fbb01749b05aec428b013c32fa5fb4d7f6
Ewa Kubik <ewa.kubik@hotmail.com>
2022-04-25 22:42:41 +1000

    update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,19 +26,21 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-      new WebpackPwaManifest ({
+      new WebpackPwaManifest({
         name: "Just Another Text Editor",
         short_name: "JATE",
+        fingerprints: false,
         description: "Text Editor",
-        start_url: '/',
-        publicPath: '/',
-        icons: [{
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        }]
-
-      })
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
