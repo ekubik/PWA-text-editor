297c3e86d23f3e79eaee77c7e14b75efb3e84ae6
Ewa Kubik <ewa.kubik@hotmail.com>
2022-04-25 21:26:00 +1000

    service worker, install, manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,9 +23,22 @@ module.exports = () => {
         title: "Text Editor",
       }),
       new InjectManifest({
-        swSrc: "./src/sw.js",
+        swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      new WebpackPwaManifest ({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor",
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        }]
+
+      })
     ],
 
     module: {
